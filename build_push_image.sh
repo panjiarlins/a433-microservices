40efad0677657933e1bb4bb90da03d95ddbbda36
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2024-12-31 12:41:41 +0700

    create docker image

---
+++ build_push_image.sh
@@ -0,0 +1,11 @@
+# Build image from Dockerfile
+docker build -t ghcr.io/username/order-service:latest .
+ 
+# Show all images
+docker image ls
+ 
+# Login to GitHub Container Registry
+echo "GITHUB_TOKEN" | docker login ghcr.io -u "username" --password-stdin
+ 
+# Push the image to the registry
+docker push ghcr.io/username/order-service:latest
